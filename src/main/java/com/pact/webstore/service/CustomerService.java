ee0c312c1b00452970a9b3136b6bb595ef44c97e
Erik Khalimov <erik.khalimov@me.com>
2015-01-03 21:27:57 +0400

    removed old files

--- src/main/java/com/pact/webstore/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.pact.webstore.service;
-
-import java.util.List;
-
-import com.pact.webstore.domain.Customer;
-
-public interface CustomerService {
-	List<Customer> getAllCustomers();
-}
--- src/main/java/com/pact/webstore/service/OrderService.java
+++
@@ -1,5 +0,0 @@
-package com.pact.webstore.service;
-
-public interface OrderService {
-	void processOrder(String productId, long quantity);
-}
