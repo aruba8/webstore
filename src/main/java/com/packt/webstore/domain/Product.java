c16a12c033b976f6f70f43c225ca9ed9f6cb021e
Erik Khalimov <biomaks@gmail.com>
2015-01-06 20:36:53 +0300

    ch9 p244

--- src/main/java/com/packt/webstore/domain/Product.java
+++ src/main/java/com/packt/webstore/domain/Product.java
@@ -1,22 +1,20 @@
 package com.packt.webstore.domain;
 
-import java.math.BigDecimal;
-
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-
+import com.packt.webstore.validator.ProductId;
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.packt.webstore.validator.ProductId;
+import javax.validation.constraints.*;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import java.io.Serializable;
+import java.math.BigDecimal;
 
 @XmlRootElement
-public class Product {
+public class Product implements Serializable{
+
+    private static final long serialVersionUID = 3678107792576131001L;
+
 	@Pattern(regexp="P[0-9]+", message="{Pattern.Product.productId.validation}")
 	@ProductId
 	private String productId;
--- src/main/java/com/packt/webstore/service/impl/OrderServiceImpl.java
+++ src/main/java/com/packt/webstore/service/impl/OrderServiceImpl.java
@@ -1,5 +1,8 @@
 package com.packt.webstore.service.impl;
 
+import com.packt.webstore.domain.Order;
+import com.packt.webstore.domain.repository.OrderRepository;
+import com.packt.webstore.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.packt.webstore.domain.Product;
@@ -11,6 +14,12 @@ public class OrderServiceImpl implements OrderService{
 
 	@Autowired
 	private ProductRepository productRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private CartService cartService;
 	
 	public void processOrder(String productId, long quantity) {
 		Product productById = productRepository.getProductById(productId);
@@ -21,4 +30,11 @@ public class OrderServiceImpl implements OrderService{
 		
 		productById.setUnitsInStock(productById.getUnitsInStock() - quantity);
 	}
+
+    @Override
+    public Long saveOrder(Order order) {
+        Long orderId = orderRepository.saveOrder(order);
+        cartService.delete(order.getCart().getCartId());
+        return orderId;
+    }
 }
